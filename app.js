e5b1e09419d22960742702c3b24e1f41ca67ab44
Cizerthapa <cizerthapa@gmail.com>
2024-06-02 10:54:41 +0545

    Adding ejs now

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Hello Cizer");
+const express = require('express');
+
+//express
+const app = express();
+
+//Listen for requests
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.sendFile('/index.html',{root : __dirname});
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile('/about.html',{root : __dirname});
+});
+
+//redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+//404 page
+app.use((req, res) => {
+    res.statusCode(404).sendFile('/404.html',{root : __dirname});
+})
